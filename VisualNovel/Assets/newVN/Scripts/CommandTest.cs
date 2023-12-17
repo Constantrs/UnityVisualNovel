901360b1a5037a849fa6c8acfce66f6a1b47ed8b
constantrs <constantrs48@gmail.com>
2023-12-17 20:07:08 +0900

    Xml解析調整

---
+++ VisualNovel/Assets/newVN/Scripts/CommandTest.cs
@@ -0,0 +1,181 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+
+[System.Serializable, XmlRoot("CommandRoot"), XmlInclude(typeof(Command))]
+public class XmlScriptData
+{
+    // コマンド
+    [XmlType("Command"), XmlInclude(typeof(CommandArgument))]
+    public class Command
+    {
+        [XmlAttribute("Name")]
+        public string name;
+
+        [XmlAttribute("GroupId")]
+        public int groupId;
+
+        [XmlArray("Arguments"), XmlArrayItem("Argument")]
+        public List<CommandArgument> arguments;
+
+        public Command()
+        {
+            name = "invalid command";
+            arguments = new List<CommandArgument>();
+        }
+
+    }
+
+    // コマンド変数
+    [XmlType("Argument")]
+    public class CommandArgument
+    {
+        //[XmlAttribute("Value")]
+        public object value;
+    }
+
+    [XmlArray("Commands"), XmlArrayItem("Command")]
+    public List<Command> commands = new List<Command>();
+}
+
+public class XmlExtension
+{
+    private static readonly string rootPath = "/newVN/Xml/";
+
+    // シリアライズ
+    #region Serializer
+    public static void Serialize<T>(string filename, T data)
+    {
+        string path = GetPath(filename);
+
+        // IDisposable継承したら、自動解放
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            var streamWriter = new StreamWriter(stream, System.Text.Encoding.UTF8);
+            serializer.Serialize(streamWriter, data);
+            Debug.Log($"Serialize XmlFile: {path}");
+        }
+    }
+    #endregion
+
+    // デシリアライズ
+    #region Deserialize
+    public static T Deserialize<T>(string filename)
+    {
+        string path = GetPath(filename);
+
+        using (var stream = new FileStream(path, FileMode.Open))
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            Debug.Log($"Deserialize XmlFile: {path}");
+            return (T)serializer.Deserialize(stream);
+        }
+    }
+    #endregion
+
+    private static string GetPath(string path)
+    {
+        string fullpath = Application.dataPath;
+        fullpath += rootPath;
+        fullpath += path;
+        return fullpath;
+    }
+}
+
+public class CommandTest : MonoBehaviour
+{
+
+    void Start()
+    {
+        //SaveXmlData();
+        
+        LoadXmlData();
+    }
+
+    private void SaveXmlData()
+    {
+        XmlScriptData data = new XmlScriptData();
+
+        XmlScriptData.Command cmd011 = new XmlScriptData.Command();
+        cmd011.name = "playSound";
+        cmd011.groupId = 1;
+        
+        {
+            XmlScriptData.CommandArgument argument1101 = new XmlScriptData.CommandArgument();
+            string strValue = "asd";
+            argument1101.value = strValue;
+            cmd011.arguments.Add(argument1101);
+        }
+        
+        XmlScriptData.Command cmd012 = new XmlScriptData.Command();
+        cmd012.name = "playEff";
+        cmd012.groupId = 1;
+        {
+            XmlScriptData.CommandArgument argument1201 = new XmlScriptData.CommandArgument();
+            int value = 3;
+            argument1201.value = value;
+            cmd012.arguments.Add(argument1201);
+        }
+
+        XmlScriptData.Command cmd013 = new XmlScriptData.Command();
+        cmd013.name = "talkMessage";
+        cmd013.groupId = 1;
+
+        {
+            XmlScriptData.CommandArgument argument1301 = new XmlScriptData.CommandArgument();
+            float valueX = 3.0f;
+            argument1301.value = valueX;
+            XmlScriptData.CommandArgument argument1302 = new XmlScriptData.CommandArgument();
+            float valueY = 3.0f;
+            argument1302.value = valueY;
+
+            cmd013.arguments.Add(argument1301);
+            cmd013.arguments.Add(argument1302);
+        }
+
+        data.commands.Add(cmd011);
+        data.commands.Add(cmd012);
+        data.commands.Add(cmd013);
+
+        XmlScriptData.Command cmd021 = new XmlScriptData.Command();
+        cmd021.name = "playBGM";
+        cmd021.groupId = 2;
+
+        XmlScriptData.Command cmd022 = new XmlScriptData.Command();
+        cmd022.name = "talkMessage";
+        cmd022.groupId = 2;
+
+        {
+            XmlScriptData.CommandArgument argument2201 = new XmlScriptData.CommandArgument();
+            int valueA = 6;
+            argument2201.value = valueA;
+            XmlScriptData.CommandArgument argument2202 = new XmlScriptData.CommandArgument();
+            int valueB = 7;
+            argument2202.value = valueB;
+            cmd022.arguments.Add(argument2201);
+            cmd022.arguments.Add(argument2202);
+        }
+
+        data.commands.Add(cmd011);
+        data.commands.Add(cmd012);
+        data.commands.Add(cmd013);
+        data.commands.Add(cmd021);
+        data.commands.Add(cmd022);
+
+        XmlExtension.Serialize<XmlScriptData>("TestXml.xml", data);
+    }
+
+    private void LoadXmlData()
+    {
+        XmlScriptData data = XmlExtension.Deserialize<XmlScriptData>("TestXml.xml");
+
+        foreach(XmlScriptData.Command command in data.commands)
+        {
+            Debug.Log(command.name);
+        }
+    }
+
+}
---
+++ VisualNovel/Assets/newVN/Scripts/DelegateTest.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+
+
+public abstract class CommandExtension
+{
+    public static void Extend(CommandDB database)
+    {
+
+    }
+}
+
+public class CommandExtension_Example : CommandExtension
+{
+    new public static void Extend(CommandDB database)
+    {
+        // Add action with no parameters
+        database.AddCommand("print", new Action(PrintDefaultMessage));
+
+        database.AddCommand("process", new Func<UniTask>(DefaultProcess));
+    }
+
+    private static void PrintDefaultMessage()
+    {
+        Debug.Log("DebugInfo");
+    }
+
+    private static async UniTask DefaultProcess()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Debug.Log($"Process Running... [{i}]");
+            await UniTask.Yield();
+        }
+    }
+}
+
+public class CommandDB
+{
+    private Dictionary<string, Delegate> database = new Dictionary<string, Delegate>();
+
+    public bool HasCommand(string commandName) => database.ContainsKey(commandName);
+
+    public void AddCommand(string commandName, Delegate command)
+    {
+        commandName = commandName.ToLower();
+
+        if (!database.ContainsKey(commandName))
+        {
+            database.Add(commandName, command);
+        }
+        else
+        {
+            Debug.LogError($"Command already exists in database! '{commandName}'");
+        }
+    }
+
+    public Delegate GetCommand(string commandName)
+    {
+        commandName = commandName.ToLower();
+
+        if (!database.ContainsKey(commandName))
+        {
+            Debug.LogError($"Command '{commandName}' does not exists in the database!");
+            return null;
+        }
+        else
+        {
+            return database[commandName];
+        }
+    }
+}
+
+public class TaskProcess
+{
+    private CancellationTokenSource _cts;
+    private PlayerLoopTiming _timing;
+    //private UniTaskCommand command;
+
+    //public TaskProcess(UniTaskCommand command)
+    //{
+    //    _cts = new CancellationTokenSource();
+    //    _timing = PlayerLoopTiming.LastEarlyUpdate;
+    //}
+
+    //public async UniTask Process(UniTaskCommand command)
+    //{
+    //    try
+    //    {
+    //        await command(_timing, _cts.Token);
+    //    }
+    //    catch (OperationCanceledException e)
+    //    {
+    //        Debug.Log(" UniTaskYieldProcess has been cancel!");
+    //    }
+    //}
+}
+
+public class DelegateTest : MonoBehaviour
+{ 
+    public static DelegateTest instance { get; private set; }
+
+    private CommandDB database;
+
+    //private List<CommandProcess> activeProcess = new List<CommandProcess>();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            database = new CommandDB();
+
+            Assembly assembly = typeof(DelegateTest).Assembly;
+            Type[] extensionType = assembly.GetTypes().Where(t => t.IsSubclassOf(typeof(CommandExtension))).ToArray();
+
+            foreach (Type extension in extensionType)
+            {
+                MethodInfo extendMethod = extension.GetMethod("Extend");
+                extendMethod.Invoke(instance, new object[] { database });
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
